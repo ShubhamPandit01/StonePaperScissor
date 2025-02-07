8bb179d68af9debcff57b9f5964c39f7229174ac
Shubham Pandit <Shubhamproduction1@gmail.com>
2025-02-07 22:36:54 +0530

    new update on (score & Reset game)

--- script.js
+++ script.js
@@ -1,53 +1,90 @@
-let arr = ["stone", "paper", "scissor"];
-let btn = document.querySelectorAll(".buttons button");
-let userScore = 0;
-let computerScore = 0;
-let userScore_span = document.getElementById("user-score");
-let computerScore_span = document.getElementById("computer-score");
-let vs = document.querySelector("#ans");
-let final = document.querySelector(".final");
+
+let arr = ["stone", "paper", "scissor"]; //creat a arr for computer move
+let btn = document.querySelectorAll(".buttons button"); // selecting all buttons
+let userScore = 0; // user score depend on the per round initial 0
+let computerScore = 0;   // computer score depend on the per round initial 0
+let userScore_span = document.getElementById("user-score"); //userscore sapn for updat the score
+let computerScore_span = document.getElementById("computer-score");   //computercore sapn for updat the score
+let vs = document.querySelector("#ans"); // ans for display the moves of user and computer
+let final = document.querySelector(".final"); // final for display the finalm result of the round
+let head = document.querySelector(".heading")  // head changing color accrding to the score
+
 
 for(let t of btn){
-    t.addEventListener("click", function(e){
+    t.addEventListener("click", function(e){  // main condition of the game where all the action going on
         let user = e.target.innerText;
         let computer = arr[parseInt(Math.random() * 3)];
-        if(user.toLowerCase() === computer){
-            vs.innerText = `${user} vs ${computer}`;
+        if(user.toLowerCase() === computer){ // for draw
+            vs.innerText = `User: ${user} VS Computer: ${computer}`
             vs.style.color = " #13335C"
             final.innerText = "It's a draw!";
             final.style.color = " #13335C";
         }
-        else if(user === "Stone" && computer === "scissor"){
+        else if(user === "Stone" && computer === "scissor"){ // user wins
             userScore++;
             userScore_span.innerText = userScore;
-            vs.innerText = `${user} vs ${computer}`;
+            vs.innerText = `User: ${user} VS Computer: ${computer}`
             vs.style.color = 'green'
             final.innerText = "You won!";
             final.style.color = "green";
         }   
-        else if(user === "Scissor" && computer === "paper"){
+        else if(user === "Scissor" && computer === "paper"){   // user wins
             userScore++;
             userScore_span.innerText = userScore;
-            vs.innerText = `${user} vs ${computer}`;
+            vs.innerText = `User: ${user} VS Computer: ${computer}`
             vs.style.color = 'green'
             final.innerText = "You won!";
             final.style.color = "green";
         }
-        else if(user === "Paper" && computer === "stone"){
+        else if(user === "Paper" && computer === "stone"){   // user wins
             userScore++;
             userScore_span.innerText = userScore;
-            vs.innerText = `${user} vs ${computer}`;
+            vs.innerText = `User: ${user} VS Computer: ${computer}`
             vs.style.color = 'green'
             final.innerText = "You won!";
             final.style.color = "green";
         }
-        else{
+        else{   // compiuter wins
             computerScore++;
             computerScore_span.innerText = computerScore;
-            vs.innerText = `${user} vs ${computer}`;
+            vs.innerText = `User: ${user} VS Computer: ${computer}`;
             vs.style.color = 'red'
             final.innerText = "You Lose";
             final.style.color = "red";
+        }
+
+        if(userScore > computerScore){ // heading changing colors according to the round winning
+            head.style.color = "green"
+        }
+        else if(userScore<computerScore){
+            head.style.color = "red"
+        }
+        else{
+            head.style.color = " #13335C"
+        }
+        
+        
+        if(userScore >= 10){
+            alert(`      !!You Won!!\n User: ${userScore} \n Computer: ${computerScore}`)
+            userScore_span.innerText = 0;  // score value 0 for restart the game
+            computerScore_span.innerText = 0;
+            userScore = 0   // score value 0 for restart the game logically
+            computerScore = 0
+            head.style.color = " #13335C"
+            vs.innerText = "Choose your hand by clicking below"
+            vs.style.color = 'black'
+            final.innerText = ''
+        } 
+        else if(computerScore >= 10){
+            alert(`      !!You Won!!\n User: ${userScore} \n Computer: ${computerScore}`)
+            computerScore_span.innerText = 0; // score value 0 for restart the game
+            userScore_span.innerText = 0; 
+            userScore = 0  // score value 0 for restart the game logically
+            computerScore = 0
+            head.style.color = " #13335C"
+            vs.innerText = "Choose your hand by clicking below"
+            vs.style.color = 'black'
+            final.innerText = ''
         }
     })
 
